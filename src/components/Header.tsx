4a1d01e3c6b5122366a4393d1e3a8d00d6409099
abigailridley <abigail.ridley@hotmail.co.uk>
2024-10-29 10:44:03 +0000

    Header CSS for accessibility and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,14 @@ export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-[#FBEAEB] border-opacity-40 bg-[#FBEAEB] bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-xl"
+        className="fixed top-0 left-1/2 h-[6rem] w-full rounded-none border border-[#FBEAEB] border-opacity-40 bg-[#FBEAEB] bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-xl"
         initial={{ x: "-50%", y: -100, opacity: 0 }}
         animate={{ x: "-50%", y: 0, opacity: 1 }}
       >
       </motion.div>
       
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-[#0303fca6] sm:w-[initial] sm:flex-nowrap sm:gap-5">
+      <nav className="flex fixed top-[0.15rem] left-1/2 h-4 -translate-x-1/2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
+        <ul className="flex w-[20rem] flex-wrap items-center justify-center pb-5  text-[0.9rem] font-medium text-[#0303fc] sm:w-[initial] sm:flex-nowrap sm:gap-5">
          
           <motion.li
             className="flex items-center justify-center "
@@ -24,7 +24,7 @@ export default function Header() {
           >
             <Link href="/" passHref className="focus:outline-none">
               <button
-                className="h-[35px] w-[35px] rounded-full  focus:outline-none focus:ring focus:ring-offset-2"
+                className="h-[35px] w-[35px] rounded-full  focus:outline-none focus:ring focus:ring-offset-2 mr-5 mt-5 sm:m-0"
               >
                 <Image
                   src="/favicon.ico"
@@ -37,7 +37,7 @@ export default function Header() {
             </Link>
           </motion.li>
 
-          {/* Navigation links */}
+        
           {links.map((link) => (
             <motion.li
               className="h-3/4 flex items-center justify-center"
