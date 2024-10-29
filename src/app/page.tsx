5854b6f01f326c973bb3e64851b004fd5d43a48a
abigailridley <abigail.ridley@hotmail.co.uk>
2024-10-29 15:58:06 +0000

    Create Section Window component
    Repeatable component for all windows used

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import About from "@/components/about";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
   return (
       <main className="flex flex-col items-center px-4">
         <Intro />
-        <SectionDivider />
+        <SectionDivider/>
         <About />
+        <SectionDivider/>
+        <Projects/>
         </main>
   );
 }
