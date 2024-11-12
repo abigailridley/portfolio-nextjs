07a5bd805f4f8c06d57c9105e20effb42543bcc8
abigailridley <abigail.ridley@hotmail.co.uk>
2024-11-12 11:41:53 +0000

    project name

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -33,7 +33,7 @@ export const projectsData = [
     imageUrl: "/weather-project.png", 
   },
   {
-    title: "Story App (Team Project)",
+    title: "Personalised Stories",
     description:
       "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting.",
     tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
