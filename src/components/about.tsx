46ff4aa32b00942e84c43f0c88e58ffadffc0ae6
abigailridley <abigail.ridley@hotmail.co.uk>
2024-11-14 16:39:12 +0000

    Update about.tsx

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -27,25 +27,31 @@ export default function About() {
       >
         <div className="font-sans p-4 space-y-4 leading-7 text-justify">
           <p>
-            To be a <span className="font-bold">full-stack developer</span> is
-            my ultimate goal, which drives me to explore a vast array of
-            programming languages and tools. Courses in{" "}
-            <span className="font-bold">Front-end development and React</span>{" "}
-            led me to enroll in an{" "}
-            <span className="font-bold">intensive Python course</span>,
-            immersing myself in{" "}
-            <span className="font-bold">back-end development</span> and testing
-            out my <span className="font-bold">Scrum master</span> skills.
-            Earning a distinction was a moment of pride that fueled me even
-            more.
+            I’m a <span className="font-bold">detail-oriented developer</span>{" "}
+            with an enthusiasm for creating{" "}
+            <span className="font-bold">
+              functional, accessible, and visually engaging applications
+            </span>
+            . My journey began in{" "}
+            <span className="font-bold">front-end development with React</span>{" "}
+            and soon expanded to full-stack work, including an{" "}
+            <span className="font-bold">intensive Python course</span> that
+            refined my <span className="font-bold">back-end skills</span> and
+            gave me hands-on experience as a{" "}
+            <span className="font-bold">Scrum master</span>. Earning a
+            distinction there was a proud moment, marking my transition into
+            more advanced projects.
           </p>
           <p>
-            During a break to travel the Americas, I built personal projects for
-            friends and family. Now with{" "}
-            <span className="font-bold">Next.js</span> catching my attention,
-            I&apos;m broadening my knowledge further with personal study in{" "}
-            <span className="font-bold">DevOps & Cloud computing</span>, as well
-            as <span className="font-bold">web design & accessibility.</span>
+            After taking time to travel across the Americas, I&apos;m now
+            refreshed and ready to launch tech my career. I&apos;ve broadened my
+            skill set with <span className="font-bold">Next.js</span>,{" "}
+            <span className="font-bold">cloud computing</span>,{" "}
+            <span className="font-bold">DevOps</span>, and a deepened focus on{" "}
+            <span className="font-bold">web design and accessibility</span>.
+            I&apos;m excited to contribute to a team where I can build on these
+            skills, delivering high-impact, user-friendly solutions for a
+            diverse audience.
           </p>
         </div>
       </SectionWindow>
