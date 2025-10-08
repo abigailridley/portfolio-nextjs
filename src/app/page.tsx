4e81fc874af34491ee801be430c8634c7a1105aa
abigailridley <abigail.ridley@hotmail.co.uk>
2025-10-08 14:17:38 +0100

    remove contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,22 @@
 import About from "@/components/about";
-import Contact from "@/components/contact";
 import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
 
-
 export default function Home() {
   return (
-      <main className="flex flex-col items-center px-4">
-        <Intro />
-        <SectionDivider/>
-        <About />
-        <SectionDivider/>
-        <Projects/>
-        <SectionDivider/>
-        <Skills/>
-        <SectionDivider/>
-        <Contact/>
-        <SectionDivider/>
-        </main>
+    <main className="flex flex-col items-center px-4">
+      <Intro />
+      <SectionDivider />
+      <About />
+      <SectionDivider />
+      <Projects />
+      <SectionDivider />
+      <Skills />
+      <SectionDivider />
+      {/* <Contact/> */}
+      <SectionDivider />
+    </main>
   );
 }
